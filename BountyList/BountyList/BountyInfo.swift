2faa745d835357d055dcdb0e94e6f1e18f8aded5
HyeongSeok <gudtjr051911@gmail.com>
2022-11-22 22:54:28 +0900

    Make model

---
+++ BountyList/BountyList/BountyInfo.swift
@@ -0,0 +1,21 @@
+//
+//  BountyInfo.swift
+//  BountyList
+//
+//  Created by ohhyeongseok on 2022/11/22.
+//
+
+import UIKit
+
+struct BountyInfo {
+    let name: String
+    let bounty: Int
+    
+    var image: UIImage? {
+        return UIImage(named: "\(name).jpg")
+    }
+    init(name: String, bounty: Int) {
+        self.name = name
+        self.bounty = bounty
+    }
+}
