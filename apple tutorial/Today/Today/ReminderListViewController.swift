1de5d3d50cdfaef35242d6cff59ee3ebda894d85
HyeongSeok <gudtjr051911@gmail.com>
2022-11-30 17:12:37 +0900

    Section 4

---
+++ apple tutorial/Today/Today/ReminderListViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by ohhyeongseok on 2022/11/30.
+//
+
+import UIKit
+
+class ReminderListViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let listLayout = listLayout()
+        collectionView.collectionViewLayout = listLayout
+        // Do any additional setup after loading the view.
+    }
+    
+    private func listLayout() -> UICollectionViewCompositionalLayout {
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
+        listConfiguration.showsSeparators = false
+        listConfiguration.backgroundColor = .clear
+        return UICollectionViewCompositionalLayout.list(using: listConfiguration)
+    }
+}
+
