3b57fc8ee386a629d255c2fe0a945bc09e808ccb
HyeongSeok <gudtjr051911@gmail.com>
2022-12-13 17:42:01 +0900

    Create project for Apple music project

---
+++ AppleMusic/AppleMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleMusic
+//
+//  Created by ohhyeongseok on 2022/12/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
