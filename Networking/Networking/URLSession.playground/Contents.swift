5a27c43a8593d8ed33d95e6016039a2f39629658
HyeongSeok <gudtjr051911@gmail.com>
2022-12-23 23:08:14 +0900

    Networking으로 실제 데이터 가져오기

--- Networking/Networking/URLSession.playground/Contents.swift
+++ Networking/Networking/URLSession.playground/Contents.swift
@@ -27,6 +27,31 @@ urlComponents.queryItems?.append(termQuery)
 let requestURL = urlComponents.url!
 
 
+struct Response: Codable {
+    let resultCount: Int
+    let tracks: [Track]
+    
+    enum CodingKeys: String, CodingKey {
+        case resultCount
+        case tracks = "results"
+    }
+}
+
+struct Track: Codable {
+    let title: String
+    let artistName: String
+    let thumbnailPath: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case artistName
+        case thumbnailPath = "artworkUrl30"
+    }
+    // trackName
+    // artistName
+    // artworkUrl30
+}
+    
 let dataTask = session.dataTask(with: requestURL) { (data, response, error) in
     guard error == nil else { return }
     
@@ -40,7 +65,32 @@ let dataTask = session.dataTask(with: requestURL) { (data, response, error) in
     
     guard let resultData = data else { return }
     let resultString = String(data: resultData, encoding: .utf8)
-    print("---> result: \(resultString)")
+    
+    // 목표: 트랙 리스트 오브젝트로 가져오기
+    // 하고 싶은 욕구 목록
+    // - Data -> Track 목록으로 가져오고 싶다 [Track]
+    // - Track 오브젝트를 만들어야겠다
+    // - Data에서 struct로 파싱하고싶다 > Codable 이용해서 만들자
+    // - Json 파일, 데이터 > 오브젝트 (Codable 이용하겠다)
+    // - Response, Track 이렇게 두 개 만들어야겠다
+    
+    // 해야할 일
+    // - Response, Track struct
+    // - struct의 프로퍼티 이름과 실제 데이터의 키와 맞추기 (Codable 디코딩하게 하기 위해서)
+    // - 파싱하기 (Decoding)
+    // - 트랙리스트 가져오기
+    
+    
+    
+    // 파싱 및 트랙 가져오기
+    do {
+        let decoder = JSONDecoder()
+        let response = try decoder.decode(Response.self, from: resultData)
+        let tracks = response.tracks
+        print("--> tracks: \(tracks.count) - \(tracks.last?.title), \(tracks.last?.thumbnailPath)")
+    } catch let error {
+        print("----> error: \(error.localizedDescription)")
+    }
 }
 
 dataTask.resume()
