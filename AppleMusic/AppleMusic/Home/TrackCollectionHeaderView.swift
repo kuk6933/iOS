38ff1e2747117e85c2b0cefcdcee7ded6c814fad
HyeongSeok <gudtjr051911@gmail.com>
2023-01-01 21:16:05 +0900

    make TrackCollectionHeaderView

---
+++ AppleMusic/AppleMusic/Home/TrackCollectionHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  TrackCollectionViewHeaderView.swift
+//  AppleMusic
+//
+//  Created by ohhyeongseok on 2023/01/01.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class TrackCollectionHeaderView: UICollectionReusableView {
+    @IBOutlet weak var thumbnailImage: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    //노래 정보 Player에 넘겨줘야 하기에 가지고 있어야함
+    
+}
