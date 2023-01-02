cb2c7633ad38fdb040a456069d17f57fea7b9d25
HyeongSeok <gudtjr051911@gmail.com>
2023-01-02 17:23:31 +0900

    Make TrackCollectionHeaderView - update - cardTapped - awarkeFromNib

--- AppleMusic/AppleMusic/Home/TrackCollectionHeaderView.swift
+++ AppleMusic/AppleMusic/Home/TrackCollectionHeaderView.swift
@@ -6,13 +6,31 @@
 //
 
 import UIKit
-
-private let reuseIdentifier = "Cell"
+import AVFoundation
 
 class TrackCollectionHeaderView: UICollectionReusableView {
-    @IBOutlet weak var thumbnailImage: UIImageView!
+    @IBOutlet weak var thumbnailImageView: UIImageView!
     @IBOutlet weak var descriptionLabel: UILabel!
     
-    //노래 정보 Player에 넘겨줘야 하기에 가지고 있어야함
+    var item: AVPlayerItem?
+    var tapHandler: ((AVPlayerItem) -> Void)?
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        thumbnailImageView.layer.cornerRadius = 4
+    }
+    
+    func update(with item: AVPlayerItem) {
+        self.item = item
+        
+        guard let track = item.convertToTrack() else { return }
+        
+        self.thumbnailImageView.image = track.artwork
+        self.descriptionLabel.text = "Today's pick is \(track.artist)'s album - \(track.albumName), Let's listen."
+    }
+    
+    @IBAction func cardTapped(_ sender: UIButton) {
+        guard let todaysItem = item else { return }
+        tapHandler?(todaysItem)
+    }
 }
