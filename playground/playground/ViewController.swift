056648eed2c23a392ab0aa90e71b1972be1710a1
HyeongSeok <gudtjr051911@gmail.com>
2023-01-12 01:16:14 +0900

    reset

--- playground/playground/ViewController.swift
+++ playground/playground/ViewController.swift
@@ -8,27 +8,5 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet var resultEmail: UILabel!
-    
-    @IBOutlet var resultIsUpdate: UILabel!
-    
-    @IBOutlet var resultInterval: UILabel!
-    
-    override func viewWillAppear(_ animated: Bool) {
-        
-        let ud = UserDefaults.standard
-        
-        if let email = ud.string(forKey: "email") {
-            resultEmail.text = email
-        }
-        
-        let update = ud.bool(forKey: "isUpdate")
-        resultIsUpdate.text = (update == true ? "자동갱신" : "자동갱신안함")
-        
-        let interval = ud.double(forKey: "interval")
-        resultInterval.text = "\(Int(interval))분마다"
-            
-    }
 }
 
